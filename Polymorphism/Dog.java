715db4c8e1946420326adc5b7f5db81fbd628eb7
Benjamin <blipscomb86@gmail.com>
2016-05-11 09:49:44 -0400

    Update Dog.java

--- Polymorphism/Dog.java
+++ Polymorphism/Dog.java
@@ -3,9 +3,6 @@
 /**
  * @author Benjamin Lipscomb
  * @version 25 Feb 2016
- * CSCI146 Algorithmic Design II
- * Dr. Brian Canada
- * Assignment 2
  *
  * CONCRETE Dog class extends the Animal superclass.
  * Defines what the Dog will say when he speaks.
